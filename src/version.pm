149c8205a0648ec6b50cd3c5e6e87efd55202203
Niels Thomas Haugård <thomas@haugaard.net>
2017-05-02 15:52:33 +0200

    automatic commit by ./remote.sh

--- src/version.pm
+++ src/version.pm
@@ -1,3 +1,3 @@
-my $version = "1.0-10";
-my $build_date = "2017-05-02 15:51";
-my $build_git_sha = "cc10b4612a9a7be8de573599fba1fd28539bf8d1";
+my $version = "1.0-16";
+my $build_date = "2017-05-02 15:52";
+my $build_git_sha = "3abcc37bfa47f768304bb431b5468bd3032cd4b5";
