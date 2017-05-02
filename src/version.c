f679b07b4b19c940c98d9858889b75f64547baaf
Niels Thomas Haugård <thomas@haugaard.net>
2017-05-02 15:51:29 +0200

    automatic commit by ./remote.sh

--- src/version.c
+++ src/version.c
@@ -1,5 +1,5 @@
-/* autogen by ./remote.sh on Man  1 Maj 2017 21:26:35 CEST */
+/* autogen by ./remote.sh on Tir  2 Maj 2017 15:51:22 CEST */
 #include "version.h"
 const char * version = "1.0-10";
-const char * build_date = "2017-05-01 21:26";
-const char * build_git_sha = "c1eb8faa958f061da237aa39c7d0a63ca0d2f2c8";
+const char * build_date = "2017-05-02 15:51";
+const char * build_git_sha = "cc10b4612a9a7be8de573599fba1fd28539bf8d1";
