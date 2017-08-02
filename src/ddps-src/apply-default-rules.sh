1ecab613e3320057db2437a8aad411b9a0d6b322
Niels Thomas Haugård <thomas@haugaard.net>
2017-08-02 15:33:33 +0200

    added/moved some files

--- src/ddps-src/apply-default-rules.sh
+++ src/ddps-src/apply-default-rules.sh
@@ -11,23 +11,38 @@
 # idea from http://nabcop.org/index.php/DDoS-DoS-attack-BCOP, conversation with Nordunet and
 # https://www.akamai.com/us/en/multimedia/documents/state-of-the-internet/q4-2016-state-of-the-internet-security-report.pdf
 
+function printrule()
+{
+	echo "$customerid;$uuid;$fastnetmoninstanceid;$administratorid;$blocktime;$dst;$src;$proto;$sport;$dport;$dport;$icmptype;$icmpcode;$tcpflags;$length;$ttl;$dscp;$fragmentencoding;$action;$description"
+	src="null"
+	proto="null"
+	sport="null"
+	dport="null"
+	dport="null"
+	icmptype="null"
+	icmpcode="null"
+	tcpflags="null"
+	length="null"
+	ttl="null"
+	dscp="null"
+	fragmentencoding="null"
+	action="null"
+	description="null"
+
+}
+
 INI=/opt/db2dps/etc/db.ini
 
 ################################################################################
 #INCLUDE_VERSION_SH
 ################################################################################
 
+# default values
 customerid="1"
 uuid="00:25:90:47:2b:48"        # <-- should be changed
 fastnetmoninstanceid="1"        # <-- should be changed
 administratorid="42"            # <-- should be changed
 ttl="null"
-
-ournetworks=`sed '/^ournetworks/!d; s/^ournetworks.*=[\t ]*//'	${INI}`
-
-# discard-udp-fragments
-proto=udp
-blocktime=1
 sport="null"
 dport="null"
 dport="null"
@@ -35,12 +50,17 @@ icmptype="null"
 icmpcode="null"
 tcpflags="null"
 length="null"
-ttl="null"
 dscp="null"
 fragmentencoding="null"
 src="null"
 action="discard"
 description="Default rule"
+
+blocktime=1
+
+#ournetworks=`sed '/^ournetworks/!d; s/^ournetworks.*=[\t ]*//'	${INI}`
+ournetworks="130.226.136.242/32"
+
 
 now=`/bin/date +%s`
 randomstr=`tr -dc A-Za-z0-9_ < /dev/urandom | head -c 8 | xargs`
@@ -57,61 +77,59 @@ for dst in $ournetworks;
 do
 	description="Default block all udp fragments"
 	fragmentencoding="[is-fragment first-fragment last-fragment]"
-	fragmentencoding="is-fragment"
-	echo "$customerid;$uuid;$fastnetmoninstanceid;$administratorid;$blocktime;$dst;$src;$proto;$sport;$dport;$dport;$icmptype;$icmpcode;$tcpflags;$length;$ttl;$dscp;$fragmentencoding;$action;$description"
-	fragmentencoding="null"
+	proto=udp
+	#fragmentencoding="is-fragment"
+	printrule
 
 	description="Default discard NTP amplification"
 	proto="udp"
 	sport="=123"
 	length="=468"
-	echo "$customerid;$uuid;$fastnetmoninstanceid;$administratorid;$blocktime;$dst;$src;$proto;$sport;$dport;$dport;$icmptype;$icmpcode;$tcpflags;$length;$ttl;$dscp;$fragmentencoding;$action;$description"
+	printrule
 
 	description="Default discard DNS amplification"
 	proto="udp"
 	sport="=53"
 	length="=512"
-	echo "$customerid;$uuid;$fastnetmoninstanceid;$administratorid;$blocktime;$dst;$src;$proto;$sport;$dport;$dport;$icmptype;$icmpcode;$tcpflags;$length;$ttl;$dscp;$fragmentencoding;$action;$description"
+	printrule
 
 	description="Default discard TCP and UDP chargen"
 	proto="=tcp =udp"
 	sport="=19"
 	length="null"
-	echo "$customerid;$uuid;$fastnetmoninstanceid;$administratorid;$blocktime;$dst;$src;$proto;$sport;$dport;$dport;$icmptype;$icmpcode;$tcpflags;$length;$ttl;$dscp;$fragmentencoding;$action;$description"
+	printrule
 
 	description="Default discard TCP and UDP QOTD"
 	proto="=tcp =udp"
 	sport="=17"
 	length="null"
-	echo "$customerid;$uuid;$fastnetmoninstanceid;$administratorid;$blocktime;$dst;$src;$proto;$sport;$dport;$dport;$icmptype;$icmpcode;$tcpflags;$length;$ttl;$dscp;$fragmentencoding;$action;$description"
+	printrule
 
 	description="Default discard IP protocol 47, GRE"
 	proto="=47"
 	sport="null"
 	length="null"
-	echo "$customerid;$uuid;$fastnetmoninstanceid;$administratorid;$blocktime;$dst;$src;$proto;$sport;$dport;$dport;$icmptype;$icmpcode;$tcpflags;$length;$ttl;$dscp;$fragmentencoding;$action;$description"
+	printrule
 
 
-	Default="Default ratelimit SSDP"
+	description="Default ratelimit SSDP"
 	action="rate-limit 9600"
 	proto="=udp"
 	sport="1900"
-	echo "$customerid;$uuid;$fastnetmoninstanceid;$administratorid;$blocktime;$dst;$src;$proto;$sport;$dport;$dport;$icmptype;$icmpcode;$tcpflags;$length;$ttl;$dscp;$fragmentencoding;$action;$description"
+	printrule
 
-	Default="Default ratelimit SNMP"
+	description="Default ratelimit SNMP"
 	action="rate-limit 9600"
 	proto="=udp"
 	sport="=161&=162"
-	echo "$customerid;$uuid;$fastnetmoninstanceid;$administratorid;$blocktime;$dst;$src;$proto;$sport;$dport;$dport;$icmptype;$icmpcode;$tcpflags;$length;$ttl;$dscp;$fragmentencoding;$action;$description"
+	printrule
 done
 
 # end
 echo "last-line"
 ) > $tmprules
 
-ls $tmprules
-
-#/bin/mv $tmprules $rulefile
+/bin/mv $tmprules $rulefile
 
 echo wrote rules to $rulefile
 
