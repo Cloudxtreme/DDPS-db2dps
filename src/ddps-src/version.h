267aa0634fe11768c0154d65c027ca787a2876d9
Niels Thomas Haugård <thomas@haugaard.net>
2017-11-06 17:14:11 +0100

    hmm

---
+++ src/ddps-src/version.h
@@ -0,0 +1,7 @@
+/* autogen by ./remote.sh on Wed Oct  4 11:31:38 CEST 2017 */
+#ifndef VERSION_H
+#define VERSION_H
+#define VERSION "1.0-91"
+extern const char * build_date;		/* date +"%Y-%m-%d %H:%M" - source file last edited */
+extern const char * build_git_sha;	/* git rev-parse HEAD */
+#endif /* VERSION_H */
---
+++ src/mkiso-src/version.c
@@ -0,0 +1,5 @@
+/* autogen by ./remote.sh on Fre  4 Aug 2017 14:57:25 CEST */
+#include "version.h"
+const char * version = "1.0-70";
+const char * build_date = "2017-08-04 14:57";
+const char * build_git_sha = "b85f7654a99072e402699aa97b7862dc61691501";
