0ad034d2a4ddb8fcb9207a4889ce5217ff344a8e
Niels Thomas Haugård <thomas@haugaard.net>
2017-11-06 17:05:22 +0100

    hmm

---
+++ src/mkiso-src/mkiso/specific/e1.deic.dk/install.d/2_install_packages.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+# General debug and vpn tools
+apt-get -y install vim tcpdump ethtool tmux openvpn unzip exabgp
+
+# Required tools for fastnetmon (install_fastnetmon.pl)
+#apt-get install -y	git autoconf automake cmake g++ gcc git libboost-all-dev	\
+#					libgeoip-dev libgpm-dev libhiredis-dev	liblog4cpp5-dev		\
+#					libncurses5-dev libnuma-dev libpcap-dev libtool pkg-config
+
